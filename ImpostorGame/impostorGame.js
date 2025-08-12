a1086ca569ea75bc86b9181b94da8aeb39dee87d
BrotJaAh1 <bagrattprogramming@gmail.com>
2025-08-12 21:19:33 +0200

    Added select option, added Food and Nature data

--- ImpostorGame/impostorGame.js
+++ ImpostorGame/impostorGame.js
@@ -1,13 +1,11 @@
-let currentIndex = 0;
-let gameList = [];
-let wordList = [['Baum', 'Vogel'], ['Apfel', 'Birne'], ['Katze', 'Tiger'], ['Stuhl', 'Sessel'], ['Fluss', 'Bach'], ['Berg', 'Hügel'], ['Brille', 'Sonnenbrille'], ['Pizza', 'Flammkuchen'],
+const Mix = [['Baum', 'Vogel'], ['Apfel', 'Birne'], ['Katze', 'Tiger'], ['Stuhl', 'Sessel'], ['Fluss', 'Bach'], ['Berg', 'Hügel'], ['Brille', 'Optiker'], ['Pizza', 'Flammkuchen'],
                 ['Schnee', 'Eis'], ['Bus', 'Straßenbahn'], ['Haus', 'Hütte'], ['Lampe', 'Laterne'], ['Schwert', 'Dolch'], ['Stern', 'Planet'], ['Wald', 'Dschungel'], ['Tee', 'Kaffee'], ['See', 'Meer'], ['Ente', 'Gans'],
-                ['Hund', 'Wolf'], ['Brot', 'Brötchen'], ['Schule', 'Universität'], ['Tür', 'Tor'], ['Krug', 'Becher'], ['Fahrrad', 'Motorrad'], ['Pferd', 'Esel'], ['Computer', 'Laptop'], ['Buch', 'Heft'],
+                ['Hund', 'Wolf'], ['Brot', 'Brötchen'], ['Schule', 'Universität'], ['Tür', 'Tor'], ['Krug', 'Becher'], ['Fahrrad', 'Justus'], ['Pferd', 'Esel'], ['Computer', 'Laptop'], ['Buch', 'Heft'],
                 ['Messer', 'Gabel'], ['Regen', 'Niesel'], ['Auto', 'Lastwagen'], ['Hose', 'Jeans'], ['Burg', 'Schloss'], ['Gitarre', 'Geige'], ['Tasche', 'Rucksack'], ['Handschuh', 'Fausthandschuh'], ['Kanu', 'Kajak'],
                 ['Sofa', 'Couch'], ['Sand', 'Staub'], ['Teller', 'Schüssel'], ['Kuchen', 'Torte'], ['Baum', 'Strauch'], ['Fisch', 'Delfin'], ['Hammer', 'Axt'], ['Löffel', 'Suppenkelle'], ['Kerze', 'Fackel'],
                 ['Brief', 'Postkarte'], ['Blume', 'Blüte'], ['Hemd', 'TShirt'], ['Wolke', 'Nebel'], ['Zug', 'Straßenbahn'], ['Kirche', 'Kapelle'], ['Brunnen', 'Quelle'], ['Turm', 'Leuchtturm'], ['Brücke', 'Steg'], 
-                ['Schloss', 'Schlüssel'], ['Bleistift', 'Kugelschreiber'], ['Sofa', 'Sessel'], ['Spiegel', 'Glas'], ['Zelt', 'Hütte'], ['Pfad', 'Straße'], ['Foto', 'Gemälde'], ['Pferd', 'Pony'],
-                ['Sofa', 'Bank'], ['Mond', 'Sonne'], ['Flasche', 'Karaffe'], ['Telefon', 'Handy'], ['Lagerfeuer', 'Kamin'], ['Zwiebel', 'Knoblauch'], ['Kartoffel', 'Süßkartoffel'], ['Kuh', 'Ochse'],
+                ['Schloss', 'Schlüssel'], ['Bleistift', 'Kugelschreiber'], ['Sofa', 'Sessel'], ['Spiegel', 'Glas'], ['Zelt', 'Hütte'], ['Pfad', 'Straße'], ['Foto', 'Gemälde'], ['Pferd', 'Jule'], 
+                ['Sofa', 'Bank'], ['Mond', 'Sonne'], ['Flasche', 'Geld'], ['Handy', 'TikTok'], ['Lagerfeuer', 'Kamin'], ['Zwiebel', 'Knoblauch'], ['Kartoffel', 'Süßkartoffel'], ['Kuh', 'Ochse'],
                 ['Seil', 'Kette'], ['Flasche', 'Dose'], ['Haifisch', 'Schwertfisch'], ['Orchester', 'Band'], ['Löwe', 'Leopard'], ['Kran', 'Bagger'], ['Fenster', 'Tür'], ['Ballon', 'Blase'], ['Hut', 'Mütze'],
                 ['Schuhe', 'Stiefel'], ['Zug', 'UBahn'], ['Flugzeug', 'Hubschrauber'], ['Brötchen', 'Croissant'], ['Keks', 'Cracker'], ['Tomate', 'Paprika'], ['Papier', 'Pappe'], ['Bürste', 'Kamm'],
                 ['Zugbrücke', 'Hängebrücke'], ['Schrank', 'Kommode'], ['Herd', 'Ofen'], ['Fensterbank', 'Regal'], ['Leiter', 'Treppe'], ['Kanone', 'Gewehr'], ['Fass', 'Tonne'], ['Glocke', 'Gong'],
@@ -24,7 +22,7 @@ let wordList = [['Baum', 'Vogel'], ['Apfel', 'Birne'], ['Katze', 'Tiger'], ['Stu
                 ['Paprika', 'Chili'], ['Schrank', 'Garderobe'], ['Strohhalm', 'Löffel'], ['Treppe', 'Rolltreppe'], ['Schublade', 'Regal'], ['Kerze', 'Duftkerze'], ['Reis', 'Couscous'], ['Apfelbaum', 'Kirschbaum'],
                 ['Segelboot', 'Motorboot'], ['Briefumschlag', 'Paket'], ['Zelt', 'Wohnwagen'], ['Ziegel', 'Stein'], ['Kaktus', 'Sukkulente'], ['Leine, Kette'], ['Boot', 'Floß'], ['Brücke', 'Aquädukt'], 
                 ['Karte', 'Atlas'], ['Wüste', 'Oase'], ['Brille', 'Schutzbrille'], ['Löwe', 'Tiger'], ['Feder', 'Pinsel'], ['Stadt', 'Dorf'], ['Post', 'Telegramm'], ['Jacke', 'Mantel'], ['Bein', 'Oberschenkel'], 
-                ['Flugzeug', 'Segelflugzeug'], ['Koffer', 'Rucksack'], ['Bank', 'Liegestuhl'], ['Schirm', 'Regenschirm'], ['Besen', 'Handfeger'], ['Bahn', 'SBahn'], ['Gabel', 'Essstäbchen'], ['Torte', 'Muffin'], 
+                ['Flugzeug', 'Bermudadreieck'], ['Koffer', 'Rucksack'], ['Bank', 'Liegestuhl'], ['Schirm', 'Regenschirm'], ['Besen', 'Handfeger'], ['Bahn', 'SBahn'], ['Gabel', 'Essstäbchen'], ['Torte', 'Muffin'], 
                 ['Stern', 'Komet'], ['Ball', 'Kugel'], ['Schuh', 'Sandale'], ['Keks', 'Waffel'], ['Tasse', 'Glas'], ['Teich', 'Pfütze'], ['Weg', 'Pfad'], ['Feuer', 'Glut'], ['Schnee', 'ReifNiederschlag'], 
                 ['Turm', 'Antenne'], ['Lager', 'Vorrat'], ['Schlange', 'Wurm'], ['Pfeil', 'Speer'], ['Notiz', 'Liste'], ['Tier', 'Insekt'], ['Radio', 'Lautsprecher'], ['Salz', 'Zucker'], 
                 ['Baumrinde', 'Holz'], ['Heizung', 'Ofen'], ['Orkan', 'Sturm'], ['Rose', 'Nelke'], ['Milch', 'Sahne'], ['Arzt', 'Chirurg'], ['Koch', 'Bäcker'], ['Bäcker', 'Konditor'], ['Zuckerwatte', 'Popcorn'], 
@@ -34,12 +32,86 @@ let wordList = [['Baum', 'Vogel'], ['Apfel', 'Birne'], ['Katze', 'Tiger'], ['Stu
                 ['Lampe', 'Laterne'], ['Flasche', 'Weinkaraffe'], ['Sonne', 'Stern'], ['Schrank', 'Vitrine'], ['Pfosten', 'Säule'], ['Glas', 'Kristallglas'], ['Uhr', 'Stoppuhr'], ['Fernseher', 'Monitor'], 
                 ['Bank', 'Tresor'], ['Plakat', 'Poster'], ['Wand', 'Mauer'], ['Stroh', 'Heu'], ['Schlüssel', 'Zahlencode'], ['Kerze', 'Öllampe'], ['Wal', 'Blauwal'], ['Hund', 'Fuchs'], ['Schal', 'Tuch'], 
                 ['Kürbis', 'Melone'], ['Brot', 'Baguette'], ['Eis', 'Sorbet'], ['Flasche', 'Milchkanne'], ['Leiter', 'Gerüst'], ['See', 'Bucht'], ['Wiese', 'Rasen'], ['Huhn', 'Truthahn'], ['Auto', 'Sportwagen'], 
-                ['Spiegel', 'Lupenspiegel'], ['Stein', 'Felsen'], ['Wolke', 'Gewitterwolke'], ['Sand', 'Kies'], ['Holz', 'Sperrholz'], ['Perle', 'Edelstein'], ['Ananas', 'Kokosnuss'], 
-                ['Zahnarzt', 'Kieferorthopäde'], ['Kleber', 'Klebeband'], ['Kamera', 'Videokamera'], ['Zelt', 'Pavillon'], ['Fenster', 'Dachfenster'], ['Hose', 'Jogginghose'], ['Korb', 'Einkaufstasche'], 
+                ['Spiegel', 'Schneewittchen'], ['Stein', 'Felsen'], ['Wolke', 'Gewitterwolke'], ['Sand', 'Kies'], ['Holz', 'Sperrholz'], ['Perle', 'Edelstein'], ['Ananas', 'Kokosnuss'], 
+                ['Zahnarzt', 'Kieferorthopäde'], ['Kleber', 'Klebeband'], ['Kamera', 'Videokamera'], ['Zelt', 'Pavillon'], ['Fenster', 'Dachfenster'], ['Hose', 'Blau'], ['Korb', 'Einkaufstasche'], 
                 ['Kissen', 'Decke'], ['Lampe', 'Kerzenhalter'], ['Ring', 'Armband'], ['Tasche', 'Einkaufskorb'], ['Salat', 'Spinat'], ['Pflaume', 'Zwetschge']];
 
-function zeigeNachricht() {
-  document.getElementById('message').textContent = "Du hast den Knopf gedrückt!";
+const Food = [['Apfel', 'Wurm'], ['Brot', 'Butter'], ['Pizza', 'Ofen'], ['Kaffee', 'Tasse'], ['Fisch', 'Angel'], ['Honig', 'Biene'], ['Milch', 'Kuh'], ['Eis', 'Sommer'], ['Salat', 'Schüssel'], ['Kartoffel', 'Acker'], 
+                ['Nudeln', 'Gabel'], ['Reis', 'Schale'], ['Kuchen', 'Kerze'], ['Bier', 'Glas'], ['Wein', 'Traube'], ['Öl', 'Pfanne'], ['Zucker', 'Löffel'], ['Tee', 'Kanne'], ['Kakao', 'Becher'], ['Butter', 'Messer'], 
+                ['Schokolade', 'Papier'], ['Bratwurst', 'Grill'], ['Hamburger', 'Pommes'], ['Suppe', 'Löffel'], ['Marmelade', 'Brot'], ['Gurke', 'Glas'], ['Käse', 'Maus'], ['Pfannkuchen', 'Pfanne'], ['Toast', 'Toaster'], 
+                ['Zitrone', 'Saft'], ['Nuss', 'Schale'], ['Banane', 'Affe'], ['Kirsche', 'Kern'], ['Melone', 'Messer'], ['Wasser', 'Flasche'], ['Cola', 'Dose'], ['Chips', 'Tüte'], ['Mais', 'Kolben'], ['Keks', 'Dose'], 
+                ['Pfeffer', 'Mühle'], ['Salz', 'Streuer'], ['Erdbeere', 'Feld'], ['Blumenkohl', 'Beet'], ['Spinat', 'Topf'], ['Pilz', 'Wald'], ['Huhn', 'Stall'], ['Ente', 'Teich'], ['Schwein', 'Stall'], ['Lamm', 'Weide'], 
+                ['Rind', 'Weide'], ['Hummer', 'Meer'], ['Krabbe', 'Strand'], ['Muschel', 'Sand'], ['Sardine', 'Dose'], ['Forelle', 'Fluss'], ['Lachs', 'Netz'], ['Ei', 'Nest'], ['Mehl', 'Sack'], ['Hefe', 'Teig'], 
+                ['Pasta', 'Italien'], ['Curry', 'Reis'], ['Sushi', 'Stäbchen'], ['Torte', 'Geburtstag'], ['Praline', 'Schachtel'], ['Popcorn', 'Kino'], ['Wurst', 'Markt'], ['Schinken', 'Schneidebrett'], ['Keks', 'Kaffee'], 
+                ['Donut', 'Zuckerguss'], ['Müsli', 'Schale'], ['Hafer', 'Feld'], ['Brot', 'Korb'], ['Käsekuchen', 'Backofen'], ['Lasagne', 'Auflaufform'], ['Steak', 'Pfanne'], ['Braten', 'Ofen'], ['Kebab', 'Spieß'], 
+                ['Falafel', 'Tasche'], ['Guacamole', 'Schüssel'], ['Hummus', 'Teller'], ['Taco', 'Soße'], ['Wrap', 'Folie'], ['Hotdog', 'Stand'], ['Smoothie', 'Mixer'], ['Limonade', 'Glas'], ['Whisky', 'Fass'], 
+                ['Gin', 'Tonic'], ['Wasser', 'Quelle'], ['Bier', 'Fass'], ['Champagner', 'Flasche'], ['Espresso', 'Maschine'], ['Latte', 'Milch'], ['Mojito', 'Minze'], ['Bloody Mary', 'Tomate'], ['Salami', 'Pizza'], 
+                ['Nudelsuppe', 'Stäbchen'], ['Paella', 'Pfanne'], ['Fondue', 'Gabel'], ['Raclette', 'Pfännchen'], ['Omelett', 'Pfanne']];
+
+const Nature = [['Baum', 'Blatt'], ['Berg', 'Tal'], ['Fluss', 'Ufer'], ['Meer', 'Welle'], ['Wald', 'Pfad'], ['Wiese', 'Blume'], ['Stein', 'Moos'], ['Himmel', 'Wolke'], ['Sonne', 'Strahl'], ['Mond', 'Stern'], 
+                ['Regen', 'Tropfen'], ['Schnee', 'Flocke'], ['Wind', 'Böe'], ['Sand', 'Düne'], ['See', 'Insel'], ['Teich', 'Libelle'], ['Bach', 'Forelle'], ['Wasserfall', 'Felsen'], ['Gletscher', 'Eis'], 
+                ['Höhle', 'Fledermaus'], ['Vulkan', 'Lava'], ['Krater', 'Asche'], ['Küste', 'Klippe'], ['Strand', 'Muschel'], ['Feld', 'Mohnblume'], ['Heide', 'Ginster'], ['Sumpf', 'Schilf'], ['Steppe', 'Gras'], 
+                ['Dschungel', 'Liane'], ['Savanne', 'Akazie'], ['Wüste', 'Oase'], ['Tundra', 'Frost'], ['Polarlicht', 'Himmel'], ['Ast', 'Nest'], ['Zweig', 'Knospe'], ['Wurzel', 'Erde'], ['Farn', 'Spore'], 
+                ['Pilz', 'Waldboden'], ['Blüte', 'Pollen'], ['Frucht', 'Samen'], ['Eiche', 'Eichel'], ['Kiefer', 'Zapfen'], ['Ahorn', 'Blatt'], ['Buche', 'Rinde'], ['Birke', 'Ast'], ['Löwenzahn', 'Samen'], 
+                ['Sonnenblume', 'Kern'], ['Rose', 'Dorn'], ['Tulpe', 'Zwiebel'], ['Lavendel', 'Duft'], ['Schmetterling', 'Flügel'], ['Biene', 'Honig'], ['Ameise', 'Hügel'], ['Käfer', 'Panzer'], ['Libelle', 'Flügel'], 
+                ['Spinne', 'Netz'], ['Vogel', 'Feder'], ['Eule', 'Augen'], ['Adler', 'Krallen'], ['Fisch', 'Schuppen'], ['Seestern', 'Arm'], ['Qualle', 'Tentakel'], ['Delfin', 'Sprung'], ['Wal', 'Fontäne'], 
+                ['Robbe', 'Flosse'], ['Fuchs', 'Bau'], ['Wolf', 'Rudel'], ['Bär', 'Höhle'], ['Hirsch', 'Geweih'], ['Reh', 'Wiese'], ['Wildschwein', 'Schlamm'], ['Eichhörnchen', 'Nuss'], ['Hase', 'Bau'], 
+                ['Igel', 'Stachel'], ['Maus', 'Feld'], ['Schlange', 'Haut'], ['Echse', 'Schwanz'], ['Frosch', 'Teich'], ['Kröte', 'Sumpf'], ['Schildkröte', 'Panzer'], ['Chamäleon', 'Farbe'], ['Papagei', 'Schnabel'], 
+                ['Pfau', 'Feder'], ['Rabe', 'Nest'], ['Krähe', 'Ast'], ['Taube', 'Flug'], ['Specht', 'Baum'], ['Storch', 'Nest'], ['Flamingo', 'Bein'], ['Pinguin', 'Eis'], ['Eisbär', 'Schnee'], ['Seehund', 'Strand'], 
+                ['Orca', 'Meer'], ['Koralle', 'Riff'], ['Alge', 'Wasser'], ['Seetang', 'Küste'], ['Lotus', 'Teich'], ['Seerose', 'Blatt'], ['Magnolie', 'Blüte']];
+
+const Cars = [];
+
+let currentIndex = 0;
+let gameList = [];
+let wordList = [];
+
+function showNext() {
+    let outputColor = document.getElementById("output");
+    if (currentIndex < gameList.length) {
+        document.getElementById("output").textContent = gameList[currentIndex];
+        currentIndex++;
+    }
+    else {
+        document.getElementById("output").textContent = "ALLE SPIELER WURDEN GEZEIGT";
+        outputColor.classList.remove('color-blue');
+        outputColor.classList.add('color-green');
+        currentIndex = 0;
+        gameList = [];
+        document.getElementById("start").style.display = "inline";
+        document.getElementById("numPlayer").style.display = "inline";
+        document.getElementById("themes").style.display = "inline";
+        document.getElementById("themeLabel").style.display = "inline";
+        document.getElementById("mode").style.display = "inline";
+        document.getElementById("modeChoice").style.display = "inline";
+    }
+}
+
+function chooseTheme(list) {
+    const theme = document.getElementById("themes").value;
+    if (theme === "mix") {
+        list = Mix;
+    }
+    else if (theme === "food") {
+        list = Food;
+    }
+    else if(theme === "nature") {
+        list = Nature;
+    }
+    else if(theme === "cars") {
+        list = Cars;
+    }
+    return list
+}
+
+function chooseMode(list) {
+    const mode = document.getElementById("modeChoice").value;
+    if (mode === "withHint") {
+        return list[1]
+    }   
+    else if (mode === "withoutHint") {
+        return ""
+    }
 }
 
 function handleInput() {
@@ -71,13 +143,18 @@ function choosePaire(list) {
 }
 
 function startButtonClicked() {
+        wordList = chooseTheme(wordList);
         let numPlayer = handleInput();
         let wordPaire = choosePaire(wordList);
         document.getElementById("start").style.display = "none";
         document.getElementById("numPlayer").style.display = "none";
+        document.getElementById("themes").style.display = "none";
+        document.getElementById("themeLabel").style.display = "none";
+        document.getElementById("mode").style.display = "none";
+        document.getElementById("modeChoice").style.display = "none";
         document.getElementById("output").style.display = "inline";
         document.getElementById("output").textContent = "Nächster Spieler";
-        gameList = createList(numPlayer, wordPaire[0], wordPaire[1]);
+        gameList = createList(numPlayer, wordPaire[0], chooseMode(wordPaire));
 }
 
 function nextButtonClicked() {
@@ -91,21 +168,4 @@ function nextButtonClicked() {
             outputColor.classList.add('color-blue');
         }
         showNext();
-}
-
-function showNext() {
-    let outputColor = document.getElementById("output");
-    if (currentIndex < gameList.length) {
-        document.getElementById("output").textContent = gameList[currentIndex];
-        currentIndex++;
-    }
-    else {
-        document.getElementById("output").textContent = "ALLE SPIELER WURDEN GEZEIGT";
-        outputColor.classList.remove('color-blue');
-        outputColor.classList.add('color-green');
-        currentIndex = 0;
-        gameList = [];
-        document.getElementById("start").style.display = "inline";
-        document.getElementById("numPlayer").style.display = "inline"; 
-    }
 }
